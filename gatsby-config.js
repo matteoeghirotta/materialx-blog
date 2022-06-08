26b72721879905e2d5c56a55e7973ca7cd65633e
Matteo Ricci <matteo.ricci@neprix.com>
2022-06-08 17:14:20 +0200

    chore: basic ga config

--- gatsby-config.js
+++ gatsby-config.js
@@ -124,31 +124,33 @@ module.exports = {
         display: `minimal-ui`,
         icon: `content/site-assets/materialx-site-icon.png` // This path is relative to the root of the site.
       }
+    },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "UA-159568966-1" // Google Analytics / GA
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        //gtagConfig: {
+          //optimize_id: "OPT_CONTAINER_ID",
+          //anonymize_ip: true,
+          //cookie_expires: 0,
+        //},
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          exclude: [],
+          // Defaults to https://www.googletagmanager.com
+          //origin: "YOUR_SELF_HOSTED_ORIGIN",
+        }
+      }
     }
-    // {
-    //   resolve: 'gatsby-transformer-remark',
-    //   options: {
-    //     plugins: [
-    //       {
-    //         resolve: 'gatsby-remark-embed-video',
-    //         options: {
-    //           // width: '100%',
-    //           // ratio: 1.77, // Optional: Defaults to 16/9 = 1.77
-    //           // height: 800, // Optional: Overrides optional.ratio
-    //           related: false, // Optional: Will remove related videos from the end of an embedded YouTube video.
-    //           noIframeBorder: true, // Optional: Disable insertion of <style> border: 0
-    //           urlOverrides: [
-    //             {
-    //               id: 'youtube',
-    //               embedURL: videoId =>
-    //                 `https://www.youtube-nocookie.com/embed/${videoId}`
-    //             }
-    //           ], // Optional: Override URL of a service provider, e.g to enable youtube-nocookie support
-    //           containerClass: 'embedVideo-container' // Optional: Custom CSS class for iframe container, for multiple classes separate them by space
-    //         }
-    //       }
-    //     ]
-    //   }
-    // }
   ]
 }
